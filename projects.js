249ff749743c9e9c99463ae93644ab10bce0d704
Antonio Ferreira <antoniofos88@gmail.com>
2025-06-07 22:31:36 -0400

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,1 +1,19 @@
-Projet
+const username = "antoniofos88";
+const projectList = document.getElementById("project-list");
+
+fetch(`https://api.github.com/users/${username}/repos?sort=updated`)
+  .then((res) => res.json())
+  .then((repos) => {
+    projectList.innerHTML = "";
+    repos.forEach((repo) => {
+      if (!repo.fork) {
+        const li = document.createElement("li");
+        li.innerHTML = `<a href="${repo.html_url}" target="_blank">${repo.name}</a>: ${repo.description || "No description"}`;
+        projectList.appendChild(li);
+      }
+    });
+  })
+  .catch((err) => {
+    projectList.innerHTML = "<li>Error loading projects.</li>";
+    console.error(err);
+  });
